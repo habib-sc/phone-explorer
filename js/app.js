1a18817215a861e61887d274aeba92c4b00a5361
habib-sc <habib97.cs@gmail.com>
2022-03-01 02:30:18 +0600

    Added sidebar details design

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ const searchPhone = () => {
         .then(dataset => displayPhone(dataset.data));
 }
 
+// Phone display function 
 const displayPhone = phones => {
     const phoneContainer = document.getElementById('phone-container');
     phones.forEach(phone => {
@@ -25,7 +26,7 @@ const displayPhone = phones => {
             <div class="card-body text text-center">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">${phone.brand}</p>
-                <button id="product-detail" class="btn fs-6 px-3 py-2 text-white" style="background-color: #5E63B6;">Get
+                <button onclick="getPhoneDetail('${phone.slug}')" class="btn fs-6 px-3 py-2 text-white" style="background-color: #5E63B6;">Get
                     Details</button>
             </div>
         </div>
@@ -33,5 +34,19 @@ const displayPhone = phones => {
         phoneContainer.appendChild(div);
         console.log(phone);
     })
-    console.log(phones);
+}
+
+// phone detail function 
+const getPhoneDetail = phoneId => {
+    const detailApiUrl = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+
+    fetch(detailApiUrl)
+        .then(response => response.json())
+        .then(dataset => displayPhoneDetail(dataset.data));
+
+}
+
+// Display phone detail function 
+const displayPhoneDetail = phoneInfo => {
+    console.log(phoneInfo);
 }
